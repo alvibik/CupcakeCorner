ddac2c01d3efd0eb863eac4b17eea8af99fb88b4
Александр Биктеев <bikteev@mail.ru>
2021-08-05 11:36:58 +0900

    Initial Commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by albik on 05.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
